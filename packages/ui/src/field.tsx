47dee7672b643315a2433d93626bc8465238ca62
gitmahin <nimahin25@gmail.com>
2025-12-03 22:45:26 +0600

    fixes bugs of ui comps

--- packages/ui/src/field.tsx
+++ packages/ui/src/field.tsx
@@ -3,9 +3,10 @@
 import { useMemo } from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "/src/lib/utils"
-import { Label } from "/src/label"
-import { Separator } from "/src/separator"
+import { cn } from "./lib/utils"
+import { Label } from "./label"
+import { Separator } from "./separator"
+
 
 function FieldSet({ className, ...props }: React.ComponentProps<"fieldset">) {
   return (
