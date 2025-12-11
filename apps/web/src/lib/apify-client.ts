92e9a100c4c65d09fa437d1bbb653a63b501547a
gitmahin <nimahin25@gmail.com>
2025-12-11 23:50:01 +0600

    removed secrets

--- apps/web/src/lib/apify-client.ts
+++ apps/web/src/lib/apify-client.ts
@@ -3,7 +3,7 @@ import { ApifyClient } from "apify-client";
 // Initialize the ApifyClient with your Apify API token
 // Replace the '<YOUR_API_TOKEN>' with your token
 export const apifyClient = new ApifyClient({
-    token: "apify_api_yUbKvPnqpbTURYA9q0yRNhBtFRxMJW0Se2vQ",
+    token: "",
 });
 
 
